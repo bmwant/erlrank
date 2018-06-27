31cf8550f039153096f6522fb6ed27a3a87f3a7e
Misha Behersky <bmwant@gmail.com>
2018-06-27 23:27:46 +0300

    Read the file :frog:

--- grading/main.erl
+++ grading/main.erl
@@ -4,5 +4,18 @@
 
 start() ->
     {ok, File} = file:open("input.txt", [read]),
+    N = erlang:list_to_integer(string:strip(io:get_line(File, ''), right, $\n)),
+    Arr = read_arr(File, N, []),
+    Res = [convert_grade(Grade) || Grade <- Arr],
+    lists:map(fun(Val) -> io:format("~B~n", [Val]) end, Res),
+    file:close(File).
 
+read_arr(File, N, Acc) when N > 0 ->
+    Num = erlang:list_to_integer(string:strip(io:get_line(File, ''), right, $\n)),
+    read_arr(File, N-1, [Num | Acc]);
+read_arr(File, _, Acc) ->
+    file:close(File),
+    lists:reverse(Acc).
 
+convert_grade(Grade) ->
+    Grade.
