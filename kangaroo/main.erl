e875e3ee899edcfdb6726a986028232dd22ff082
Misha Behersky <bmwant@gmail.com>
2018-07-05 13:16:04 +0300

    Do not know how to handle zero division :interrobang:

--- kangaroo/main.erl
+++ kangaroo/main.erl
@@ -2,15 +2,25 @@
 -module(main).
 -export([start/0]).
 
+is_round(Val) ->
+    if
+        floor(Val) == Val, Val >= 0 ->
+            true;
+        true ->
+            false
+    end.
+
 start() ->
-    {ok, File} = file:open("input.txt", [read]),
+    {ok, File} = file:open("input03.txt", [read]),
     Line = string:lexemes(string:strip(io:get_line(File, ''), right, $\n), " "),
     [X1, V1, X2, V2]= [erlang:list_to_integer(Val) || Val <- Line],
-    End = (X1*V2 - X2*V1) / (V2 - V1),
-    if 
-        floor(End) == End, End >= 0 ->
-           io:format("YES~n");
-        true ->
+    End = try (X1*V2 - X2*V1) / (V2 - V1)
+        catch _:_ -> 0 end,
+    Steps = (End - X2) / V2,
+    case {is_round(End), is_round(Steps)} of
+        {true, true} ->
+            io:format("YES~n");
+        _Else ->
             io:format("NO~n")
     end,
     file:close(File).
