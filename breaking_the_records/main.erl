325fa6ce3e7b784bd78a2b59f2e460da0b85b004
Misha Behersky <bmwant@gmail.com>
2018-07-09 15:18:43 +0300

    Solve breaking the records :satellite:

--- breaking_the_records/main.erl
+++ breaking_the_records/main.erl
@@ -3,4 +3,25 @@
 -export([start/0]).
 
 start() ->
-    ok.
+    {ok, File} = file:open("input.txt", [read]),
+    _ = io:get_line(File, ''),
+    Records = nums(io:get_line(File, '')),
+    [Initial | Rest] = Records,
+    {MinB, MaxB} = count(Rest, Initial, Initial, 0, 0),
+    io:format("~p ~p~n", [MaxB, MinB]),
+    file:close(File).
+
+nums(Line) ->
+    [erlang:list_to_integer(Val) || Val <- string:lexemes(string:strip(Line, right, $\n), " ")].
+
+count([H|Rest], Min, Max, MinB, MaxB) ->
+    case H of
+        H when H < Min ->
+            count(Rest, H, Max, MinB+1, MaxB);
+        H when H > Max ->
+            count(Rest, Min, H, MinB, MaxB+1);
+        _Else ->
+            count(Rest, Min, Max, MinB, MaxB)
+    end;
+count([], _Min, _Max, MinB, MaxB) ->
+    {MinB, MaxB}.
