70d978d1229f34d6418813cf9f8fabdb2eeff844
Misha Behersky <bmwant@gmail.com>
2018-07-23 23:00:37 +0300

    Add some tests :circus_tent:

--- pitonio/generate_files/tests.py
+++ pitonio/generate_files/tests.py
@@ -18,19 +18,29 @@ class TestGeneratingContent(unittest.TestCase):
         self.assertEqual(content, self.BUFFER)
 
     def test_divisible_by_five(self):
-        pass
+        content = get_content_by_number(25, self.BUFFER)
+        self.assertEqual(content, self.FIFTH_LINE)
 
     def test_divisible_by_seven(self):
-        pass
+        content = get_content_by_number(49, self.BUFFER)
+        self.assertEqual(content, self.BUFFER)
 
     def test_divisible_both_by_five_and_seven(self):
-        pass
+        # should ignore previous rules and return concatenated content of
+        # previous files
+        content = get_content_by_number(35, self.BUFFER)
+        self.assertEqual(content, self.BUFFER)
 
     def test_random_string(self):
         pass
 
     def test_call_without_buffer(self):
-        pass
+        """
+        Function should always be called with a buffer provided to return
+        content of previous files if needed.
+        """
+        with self.assertRaises(TypeError):
+            get_content_by_number(14)
 
 
 if __name__ == '__main__':
